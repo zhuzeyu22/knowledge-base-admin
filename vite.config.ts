76e1a3cc0fd7584ce7a317f8e3af89daafffb4d5
zhuzeyu <zhuzeyu@qq.com>
2025-10-11 16:23:26 +0800

    feat: 增加环境相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/console": {
+        target: "http://www.finna.com.cn",
+        changeOrigin: true,
+      },
+      "/tenant": {
+        target: "http://www.finna.com.cn",
+        changeOrigin: true,
+      },
+    },
+  },
+});
