ef341aa7f00603d1ac112a115e14514bb3bf4cdc
chenjia-min <1766798020@qq.com>
2025-10-13 17:44:39 +0800

    暂时删除了pinia

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,9 @@ import 'element-plus/dist/index.css'
 import router from './router'
 import './style.css'
 import '../src/assets/less/index.less'
-import { createPinia } from 'pinia'
 
 const app = createApp(App)
-const pinia = createPinia()
 
 app.use(router)
 app.use(ElementPlus)
-app.use(pinia)
 app.mount('#app')
